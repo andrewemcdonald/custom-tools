3fe2de404f3396a9619877f1a5fe91059d7112bf
andrew <andrewemcdonald@outlook.com>
2019-12-31 16:23:20 -0800

    set up python utilities

---
+++ python_progs/mapit.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+
+import sys, webbrowser, pyperclip
+
+# Gather address information
+if (len(sys.argv) > 1):
+    addr = ' '.join(sys.argv[1:])
+else:
+    addr = pyperclip.paste()
+
+# Do the search
+webbrowser.open('https://www.google.com/maps/search/' + addr)
